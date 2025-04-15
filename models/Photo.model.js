999355573e0ebfb40c0688bdd3ddd9f9ee2f06ec
Monika Grzanek <grzanekmonika@o2.pl>
2025-04-15 10:04:44 +0200

    Update photoSchema

--- models/Photo.model.js
+++ models/Photo.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  email: { type: String, required: true },
+  title: { type: String, minlength: 3, maxlength: 25, match: /^[A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż -.,]+$/, required: true },
+  author: { type: String, minlength: 3, maxlength: 50, match: /^[A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż -.,]+$/, required: true },
+  email: { type: String, match: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, required: true },
   src: { type: String, required: true },
   votes: { type: Number, required: true },
 });
